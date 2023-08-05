015628237fe88ba4ee877133eca3a5417d9b9600
Rushifaaa <jakubgencer@protonmail.com>
2023-08-05 02:08:01 +0200

    Formatted Networking

--- MangaReader/Networking/Networking.swift
+++ MangaReader/Networking/Networking.swift
@@ -1,51 +1,49 @@
 import Foundation
 import SwiftUI
 #if canImport(UIKit)
-import UIKit
+    import UIKit
 #elseif canImport(AppKit)
-import AppKit
+    import AppKit
 #endif
 
 enum API: String {
     case trending = "/top"
-    case search = "/v1.0/search/?page=1&limit=50&tachiyomi=true&t=false"
-    
+    case search = "/v1.0/search/?page=1&limit=8&tachiyomi=true&t=false"
+
     private var url: URL {
         let baseURL = "https://api.comick.fun"
-        return URL(string: baseURL + self.rawValue)!
+        return URL(string: baseURL + rawValue)!
     }
-    
+
     // Make support for advanced requests like with params
     func request<T: Decodable>() async throws -> [T] {
-        let (data, _) = try await URLSession.shared.data(from: self.url)
+        let (data, _) = try await URLSession.shared.data(from: url)
         let fetchedData = try JSONDecoder().decode([T].self, from: data)
-        
+
         return fetchedData
     }
-    
+
     static func loadImage(with id: String) async throws -> Image {
         let imageURL = URL(string: "https://meo.comick.pictures/\(id)")!
         let (data, _) = try await URLSession.shared.data(from: imageURL)
-        
+
         #if os(iOS)
-        let fetchedImage = UIImage(data: data)
-        guard let fetchedImage else { return Image(systemName: "exclamationmark.triangle.fill") }
-        return Image(uiImage: fetchedImage)
+            let fetchedImage = UIImage(data: data)
+            guard let fetchedImage else { return Image(systemName: "exclamationmark.triangle.fill") }
+            return Image(uiImage: fetchedImage)
         #elseif os(macOS)
-        let fetchedImage = NSImage(data: data)
-        guard let fetchedImage else { return Image(systemName: "exclamationmark.triangle.fill") }
-        return Image(nsImage: fetchedImage)
+            let fetchedImage = NSImage(data: data)
+            guard let fetchedImage else { return Image(systemName: "exclamationmark.triangle.fill") }
+            return Image(nsImage: fetchedImage)
         #endif
-        
     }
 }
 
 class Networking {
-    
     public static let shared = Networking()
     private init() {}
-    
+
     func getAllMangas() async throws -> [Manga] {
-        return try await API.search.request()
+        try await API.search.request()
     }
 }
