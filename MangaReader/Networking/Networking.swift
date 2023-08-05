a241b8049124afc8ade59ea03f1764cc395b9418
Rushifaaa <jakubgencer@protonmail.com>
2023-08-05 06:01:23 +0200

    Added advanced search request

--- MangaReader/Networking/Networking.swift
+++ MangaReader/Networking/Networking.swift
@@ -15,9 +15,17 @@ enum API: String {
         return URL(string: baseURL + rawValue)!
     }
 
-    // Make support for advanced requests like with params
-    func request<T: Decodable>() async throws -> [T] {
-        let (data, _) = try await URLSession.shared.data(from: url)
+    func request<T: Decodable>(param: [String: [String]]? = nil) async throws -> [T] {
+        var editedURL = url
+        
+        if let param, var components = URLComponents(url: url, resolvingAgainstBaseURL: true) {
+            components.queryItems = param.map { URLQueryItem(name: $0.key, value: $0.value.joined(separator: ",")) }
+            if let urlWithParams = components.url {
+                editedURL = urlWithParams
+            }
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: editedURL)
         let fetchedData = try JSONDecoder().decode([T].self, from: data)
 
         return fetchedData
@@ -45,5 +53,97 @@ class Networking {
 
     func getAllMangas() async throws -> [Manga] {
         try await API.search.request()
+    }
+    
+    enum SearchType: String {
+        case author
+        case user
+        case group
+        case comic
+        case none = ""
+    }
+    
+    enum SearchSortType: String {
+        case follow
+        case view
+        case createdAt = "created_at"
+        case uploaded
+        case rating
+        case userFollowCount = "user_follow_count"
+    }
+    
+    func search(with genres: [String] = [],
+                excludes: [String] = [],
+                type: SearchType = .none,
+                tags: [String] = [],
+                demographic: [Int] = [],
+                page: Int = 1,
+                limit: Int = 30,
+                time: Int? = nil,
+                country: [String] = [],
+                minChapterCount: Int? = nil,
+                fromYear: Int? = nil,
+                toYear: Int? = nil,
+                status: MangaStatus? = nil,
+                tachiyomi: Bool = true,
+                completed: Bool? = nil,
+                sort: SearchSortType?,
+                excludeMyList: Bool? = nil,
+                searchString: String = "",
+                showAltTitle: Bool? = nil) async throws -> [Manga] {
+        
+        var params: [String: [String]] = [:]
+        
+        if searchString.isEmpty {
+            params["genres"] = genres
+            params["excludes"] = excludes
+            params["tags"] = tags
+            params["demograpic"] = demographic.map { String($0) }
+            params["page"] = [String(page)]
+            params["limit"] = [String(limit)]
+            params["country"] = country
+            
+            if let time {
+                params["time"] = [String(time)]
+            }
+            
+            if let minChapterCount {
+                params["minimum"] = [String(minChapterCount)]
+            }
+            
+            if let fromYear {
+                params["from"] = [String(fromYear)]
+            }
+            
+            
+            if let toYear {
+                params["to"] = [String(toYear)]
+            }
+            
+            if let status {
+                params["from"] = [String(status.rawValue)]
+            }
+            
+            if let completed {
+                params["completed"] = [String(completed)]
+            }
+            
+            if let sort {
+                params["sort"] = [sort.rawValue]
+            }
+            
+            if let excludeMyList {
+                params["exclude-mylist"] = [String(excludeMyList)]
+            }
+            
+            if let showAltTitle {
+                params("t") = [String(showAltTitle)]
+            }
+            
+        } else {
+            params["q"] = [searchString]
+            return try await API.search.request(param: params)
+        }
+        return return try await API.search.request(param: params)
     }
 }
